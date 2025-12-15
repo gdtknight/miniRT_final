f8dcf97f04aa6980faba32d57b21936670acbaa6
yoshin <yoshin@student.42gyeongsan.kr>
2025-12-15 15:59:50 +0900

    feat: Add vector.c - Basic vector operations
    Functions implemented:
    - vec3_add: Add two vectors
    - vec3_subtract: Subtract two vectors
    - vec3_multiply: Multiply vector by scalar
    - vec3_divide: Divide vector by scalar
    
    All functions follow proper vector mathematics.

---
+++ src/math/vector.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: miniRT team <miniRT@42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/15 00:00:00 by miniRT           #+#    #+#             */
+/*   Updated: 2025/12/15 00:00:00 by miniRT          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "vec3.h"
+
+t_vec3	vec3_add(t_vec3 a, t_vec3 b)
+{
+	t_vec3	result;
+
+	result.x = a.x + b.x;
+	result.y = a.y + b.y;
+	result.z = a.z + b.z;
+	return (result);
+}
+
+t_vec3	vec3_subtract(t_vec3 a, t_vec3 b)
+{
+	t_vec3	result;
+
+	result.x = a.x - b.x;
+	result.y = a.y - b.y;
+	result.z = a.z - b.z;
+	return (result);
+}
+
+t_vec3	vec3_multiply(t_vec3 v, double scalar)
+{
+	t_vec3	result;
+
+	result.x = v.x * scalar;
+	result.y = v.y * scalar;
+	result.z = v.z * scalar;
+	return (result);
+}
